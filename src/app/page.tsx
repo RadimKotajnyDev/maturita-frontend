261940e62565f4b33c55536b68e537d4c121607f
Radim Kotajny <radimkotajny@gmail.com>
2023-11-23 12:47:48 +0100

    added axios post request to search query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@
 import {motion} from "framer-motion"
 import bgImage from "public/backgroundImage.jpg"
 import Image from "next/image";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import {SearchBar} from "@/components/SearchBar";
 import {BlueOverlay} from "@/components/BlueOverlay";
+import axios from "axios"
+import {usePathname} from "next/navigation";
+
 
 export default function Home() {
 
-  const [query, setQuery] = useState<string>();
-  useEffect(() => {
-    console.log(query)
-  }, [query])
+  const [query, setQuery] = useState<string>("");
+
+  const pathname = usePathname();
+  async function SendForm(text: string) {
+    axios.post(`${pathname}/search?=${text}`, text)
+    .then((r) => console.log(r))
+  }
 
   return (
     <motion.main
@@ -19,10 +25,11 @@ export default function Home() {
       animate={{opacity: 1}}
       transition={{duration: 2, delay: .25}} className="relative w-full h-screen">
       <Image src={bgImage} alt="Background Image" className="w-full h-full object-cover" />
-
       <BlueOverlay />
 
-      <SearchBar searchBarChange={(e) => setQuery(e.target.value)} />
+      <form onSubmit={() => SendForm(query)}>
+        <SearchBar searchBarChange={(e) => setQuery(e.target.value)} />
+      </form>
     </motion.main>
   )
 }
